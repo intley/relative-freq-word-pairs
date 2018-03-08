ca35d6b062213dfa4193e328d64e2d99a521ff0e
Intley <intley@gmail.com>
2018-03-08 13:46:52 -0500

    Config changes

--- src/CountWords.java
+++ src/CountWords.java
@@ -5,17 +5,13 @@
  */
 
 // Java libraries
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 // Configuration libraries required for Hadoop & MapReduce
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.Job;
@@ -23,7 +19,6 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
 
 public class CountWords extends Configured implements Tool {
 
